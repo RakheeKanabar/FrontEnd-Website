de770999f4dd38b75c83ab6c58d000abbb0a95be
RakheeKanabar <p15203891@my365.dmu.ac.uk>
2017-12-13 20:45:17 +0000

    Re-design
    as the old layout could not be responsives. i created a new layout.
    new navigation and sections
    home page is completed.
    education page is in progress..

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+(function(){
+
+  let menuButton = document.getElementById("menu");
+  let navMenu = document.getElementById("nav-menu");
+
+  menuButton.addEventListener("click", toggleMenu);
+
+  let toggle = false; // hidden at first
+  function toggleMenu(){
+    if (toggle) { // true: it's visible
+      navMenu.classList.remove("show-menu"), // hide it
+      toggle = false
+    }
+    else { // false: it's hidden
+      navMenu.classList.add("show-menu"), // show it
+      toggle = true
+    }
+  }
+
+})();
+
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
